61422a15fb4de4f54de89b7d6acf8f14dcd0e0fa
armand <olivares12@hotmail.com>
2021-07-27 07:51:53 -0400

    Add create sentence script

---
+++ createsentens.js
@@ -0,0 +1,44 @@
+
+
+function createSentence(){
+    var self = this;
+    return{
+        words: [],
+        add(str){
+                str.split(' ').forEach(word => {
+                const trimWord = word.trim();
+
+                if (trimWord !== ''){
+                    this.words.push(trimWord);
+                    // console.log(this.words, "tr" );
+                }
+            });
+            // console.log(this.words, "h");
+            return this
+        },
+        get(){
+            console.log(this.words, "h");
+            return this.words.join(" ") + ".";
+        }
+    }
+
+}
+
+
+function createLower(){
+    const sentence = createSentence();
+    return {
+        add(str){
+            
+            sentence.add(str.toLowerCase());
+        },
+        get: () => sentence.get()
+    }
+}
+
+let  n = createLower();
+n.add("Mola Mundo");
+
+// console.log(createSentence().add("Mola Mundo"));
+
+console.log(n.get());
