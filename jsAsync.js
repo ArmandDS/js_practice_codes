88d57cff3c99ff5726d7f7728a0e869e4d28ecc4
armand <olivares12@hotmail.com>
2021-07-31 09:51:01 -0400

    Added async demo

---
+++ jsAsync.js
@@ -0,0 +1,15 @@
+
+
+var arr = ['first', 'second', 'third', 'fourth'];
+
+for (var i= 0; i<3; i++){
+    console.log(i);
+    setTimeout(()=>{
+        console.log(arr[i]);
+    }, 2500)
+}
+
+
+//fourt
+//fourt
+//fourt
