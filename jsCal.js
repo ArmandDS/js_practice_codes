bd5a52496de4e8b3373791cb60fdc981f2e5784a
armand <olivares12@hotmail.com>
2021-08-05 18:57:02 -0400

    add simple calculator example

---
+++ jsCal.js
@@ -0,0 +1,41 @@
+
+class calculator {
+
+    constructor(starVal = 0){
+        this.val = starVal;
+    }
+    result(){
+        return this.val;
+    }
+
+}
+
+calculator.prototype.add = function(num){
+    this.val +=num;
+    console.log(this.result());
+    return this;
+}
+
+
+calculator.prototype.mult = function(num){
+    this.val *=num;
+    console.log(this.result());
+    return this;
+}
+
+
+calculator.prototype.do = function(ope, num){
+
+    if ( ope  === "+"){
+        this.add(num);
+    }else if (ope === "*"){
+        this.mult(num)
+    }
+}
+
+
+const cal = new calculator(0);
+
+// cal.do("+", 2).add(2).do("*", 2).mult(2);
+
+console.log(cal)
