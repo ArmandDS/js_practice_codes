606f9ef2f74c5351d0f3ee18e69afae7269f4a19
armand <olivares12@hotmail.com>
2021-08-09 07:24:59 -0400

    add dict basic example

---
+++ jscore1.js
@@ -0,0 +1,18 @@
+function getNormUser(user){
+    // user.username = user.username.toLowerCase();
+    // return {...user, username: user.username.toLowerCase(), id:30};
+    const user1 =  JSON.parse(JSON.stringify(user));
+    user1.username = user1.username.toLowerCase();
+    return user1
+}
+
+
+let u = {
+    id: 12,
+    username: 'SomeUSer',
+};
+
+let u2 = getNormUser(u);
+
+
+console.log(u, u2)
