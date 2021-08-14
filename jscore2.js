af2c2d33849de2cf3f05db7ad078066cb6824339
armand <olivares12@hotmail.com>
2021-08-14 12:34:20 -0400

    add score2

---
+++ jscore2.js
@@ -0,0 +1,28 @@
+
+function sayHi(fullname, callback){
+    console.log(`Hi, ${fullname}!`);
+    if ( typeof callback === 'function'){
+        callback();
+    }
+}
+
+function sayBye(fullname, callback){
+    console.log(`Bye, ${fullname}!`);
+    callback();
+   
+}
+
+function printMessage(firstname, lastname, callback){
+    const fullname = `${firstname} ${lastname}`
+    console.log(`Hi, ${fullname}!`);
+    if ( typeof callback === 'function'){
+        callback(fullname);
+    }
+}
+
+
+printMessage('jon', 'snow', x => console.log(x));
+
+
+printMessage('mark', 'newman', sayHi);
+
