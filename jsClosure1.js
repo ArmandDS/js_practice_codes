4a30387a03e6a20b332a4d7b77b96702139ce7ee
Lint Action <lint-action@samuelmeuli.com>
2021-08-14 17:50:16 +0000

    A Prettier issue was resolved automatically

--- jsClosure1.js
+++ jsClosure1.js
@@ -1,22 +1,22 @@
 function math(operation, x) {
   const OPERATION = {
-    '*': (a, b) => a * b,
-    '/': (a, b) => a / b,
-    '+': (a, b) => a + b,
-    '-': (a, b) => a - b,
+    "*": (a, b) => a * b,
+    "/": (a, b) => a / b,
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
   };
 
-  return function(y) {
+  return function (y) {
     return OPERATION[operation](x, y);
   };
 }
 
-const mul = math('*', 5);
+const mul = math("*", 5);
 
-const add = math('+', mul(2));
+const add = math("+", mul(2));
 
 console.log(typeof add);
 
-console.log(add(math('-', 25)(20)));
+console.log(add(math("-", 25)(20)));
 
 // Function and 15
