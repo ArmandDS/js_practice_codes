e4ee4ef624d2b5b4b6556e866b17ecc5195b4054
Lint Action <lint-action@samuelmeuli.com>
2021-08-21 22:20:15 +0000

    A Prettier issue was resolved automatically

--- jsCoreThis.js
+++ jsCoreThis.js
@@ -1,20 +1,16 @@
-
 // Nro 18
-
 
 const obj = {
   sum: 0,
   addMul(nums) {
-    nums.forEach((x)=>{
-      this.sum +=x;
+    nums.forEach((x) => {
+      this.sum += x;
     });
   },
 };
 
 console.log(obj.addMul([1, 2, 3]));
 
-
 console.log(obj.sum);
-
 
 // //Replace wih arrow
