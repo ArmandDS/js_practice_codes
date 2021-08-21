06483796f6b973a4035981fd19456e5f7f54acc8
armand <olivares12@hotmail.com>
2021-08-21 18:19:29 -0400

    add core this

---
+++ jsCoreThis.js
@@ -0,0 +1,20 @@
+
+// Nro 18
+
+
+const obj = {
+    sum:0,
+    addMul(nums){
+        nums.forEach((x)=>{
+            this.sum +=x;
+        });
+    }
+}
+
+console.log(obj.addMul([1,2,3]))
+
+
+console.log(obj.sum)
+
+
+////Replace wih arrow
