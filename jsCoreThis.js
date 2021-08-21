6002bf594ad8d0461fff76f2c28c195794c99a50
Lint Action <lint-action@samuelmeuli.com>
2021-08-21 22:20:14 +0000

    A ESLint issue was resolved automatically

--- jsCoreThis.js
+++ jsCoreThis.js
@@ -3,18 +3,18 @@
 
 
 const obj = {
-    sum:0,
-    addMul(nums){
-        nums.forEach((x)=>{
-            this.sum +=x;
-        });
-    }
-}
+  sum: 0,
+  addMul(nums) {
+    nums.forEach((x)=>{
+      this.sum +=x;
+    });
+  },
+};
 
-console.log(obj.addMul([1,2,3]))
-
-
-console.log(obj.sum)
+console.log(obj.addMul([1, 2, 3]));
 
 
-////Replace wih arrow
+console.log(obj.sum);
+
+
+// //Replace wih arrow
