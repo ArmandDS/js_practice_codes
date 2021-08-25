3c998ef79c219e8b6d9c1f20e03c191055567127
Lint Action <lint-action@samuelmeuli.com>
2021-08-25 10:42:32 +0000

    A ESLint issue was resolved automatically

--- jsHoist.js
+++ jsHoist.js
@@ -2,10 +2,10 @@
 
 printNum(150);
 
-function printNum(){
-    console.log(num, '10');
-    var num = 5;
-    console.log(num, '10');
+function printNum() {
+  console.log(num, '10');
+  var num = 5;
+  console.log(num, '10');
 }
 
-'undefined'
+'undefined';
