b581dd4428e8ea2036a219d8b0ec107058c5d20a
Lint Action <lint-action@samuelmeuli.com>
2021-09-08 23:39:51 +0000

    A Prettier issue was resolved automatically

--- jsScope.js
+++ jsScope.js
@@ -1,9 +1,9 @@
-
 const counter2 = {
   next: 1,
   tick() {
-    setTimeout(()=>{
-      console.log(this.next); this.next++;
+    setTimeout(() => {
+      console.log(this.next);
+      this.next++;
     }, 1000);
   },
 };
@@ -11,12 +11,11 @@ const counter2 = {
 const counter = {
   next: 1,
   tick() {
-    const f1 = (n) =>setTimeout(()=>console.log(n), 1000);
+    const f1 = (n) => setTimeout(() => console.log(n), 1000);
     f1(this.next);
     this.next++;
   },
 };
-
 
 // // move this.next++ into the timeout
 counter.tick();
