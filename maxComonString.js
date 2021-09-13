602958787f03f2a3e0c9c6d46de7fb44ec08fabf
Lint Action <lint-action@samuelmeuli.com>
2021-09-13 19:20:38 +0000

    A Prettier issue was resolved automatically

--- maxComonString.js
+++ maxComonString.js
@@ -1,42 +1,41 @@
-
 function maxComS(words, k) {
   let matrix_1 = [];
   let longest = 0;
   let c = 0;
-  let long_str = '';
+  let long_str = "";
   const result = [];
-  const resultado= {};
+  const resultado = {};
   words.forEach((element) => {
     longest = 0;
     c = 0;
     // long_str = '';
-    words.forEach((ele) =>{
-      const m = element.length+ 1;
-      const n = ele.length+1;
-      long_str = '';
+    words.forEach((ele) => {
+      const m = element.length + 1;
+      const n = ele.length + 1;
+      long_str = "";
       longest = 0;
       c = 0;
       // console.log(m,n);
       matrix_1 = [];
-      for (let i=0; i<m; i++ ) {
+      for (let i = 0; i < m; i++) {
         matrix_1[i] = [];
-        for (let j= 0; j< n; j++) {
+        for (let j = 0; j < n; j++) {
           matrix_1[i][j] = 0;
         }
       }
       // console.log(matrix_1);
-      for (let i=0; i< m-1; i++) {
-        for (let j = 0; j< n-1; j++) {
+      for (let i = 0; i < m - 1; i++) {
+        for (let j = 0; j < n - 1; j++) {
           if (element[i] == ele[j]) {
             c = matrix_1[i][j] + 1;
             // console.log(c);
-            matrix_1[i+1][j+1] = c;
+            matrix_1[i + 1][j + 1] = c;
             if (c >= longest) {
               if (c == longest) {
-                long_str += ' '+ element.slice(i-c+1, i+1);
+                long_str += " " + element.slice(i - c + 1, i + 1);
               }
               longest = c;
-              long_str = element.slice(i-c+1, i+1);
+              long_str = element.slice(i - c + 1, i + 1);
             }
           }
         }
@@ -50,13 +49,13 @@ function maxComS(words, k) {
     });
   });
   console.log(result);
-  result.forEach((res) =>{
-    words.forEach((wres) =>{
+  result.forEach((res) => {
+    words.forEach((wres) => {
       if (wres != res) {
         // console.log(res, wres);
         if (wres.includes(res)) {
           if (resultado[res]) {
-            resultado[res] ++;
+            resultado[res]++;
           } else {
             resultado[res] = 1;
           }
@@ -70,27 +69,27 @@ function maxComS(words, k) {
   const result_filter = [];
 
   max_val.forEach((ele) => {
-    if (resultado[ele] >=k) {
+    if (resultado[ele] >= k) {
       result_filter.push(ele);
     }
   });
 
   // console.log(result_filter);
   let max_letter = [];
-  if ( result_filter.length != 1) {
-    result_filter.sort((a, b) =>{
+  if (result_filter.length != 1) {
+    result_filter.sort((a, b) => {
       if (a < b) return -1;
       return 0;
     });
 
     let max_split_letters = 0;
-    result_filter.forEach((elem) =>{
-      if (elem.toString().split('').length >= max_split_letters) {
-        max_split_letters = elem.toString().split('').length;
+    result_filter.forEach((elem) => {
+      if (elem.toString().split("").length >= max_split_letters) {
+        max_split_letters = elem.toString().split("").length;
       }
     });
-    max_letter = result_filter.filter((elem) =>{
-      if (elem.toString().split('').length == max_split_letters) {
+    max_letter = result_filter.filter((elem) => {
+      if (elem.toString().split("").length == max_split_letters) {
         return elem;
       }
     });
@@ -101,10 +100,18 @@ function maxComS(words, k) {
   return result_filter[0];
 }
 
+console.log(maxComS(["dragon", "rage", "age", "savage"], 2));
 
-console.log(maxComS(['dragon', 'rage', 'age', 'savage'], 2));
+console.log(maxComS(["aaaaa", "aabbbaa", "bbbaabbb", "ccc", "ccbbbc"], 3));
 
-
-console.log(maxComS(['aaaaa', 'aabbbaa', 'bbbaabbb', 'ccc', 'ccbbbc'], 3));
-
-console.log(maxComS(['luclnyuvojoculuaielduaudv', 'dwrsztqgvqadsfepqwlfmdliw', 'mdvfkozievilwxweczgwcvvbo', 'qbefugwzqgdwmolqibbyyzdvm'], 3));
+console.log(
+  maxComS(
+    [
+      "luclnyuvojoculuaielduaudv",
+      "dwrsztqgvqadsfepqwlfmdliw",
+      "mdvfkozievilwxweczgwcvvbo",
+      "qbefugwzqgdwmolqibbyyzdvm",
+    ],
+    3
+  )
+);
