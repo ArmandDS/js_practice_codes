28ee4799181ac3c9ceabc30fb22198990055c732
Lint Action <lint-action@samuelmeuli.com>
2021-09-15 11:13:46 +0000

    A Prettier issue was resolved automatically

--- orderingTaxi.js
+++ orderingTaxi.js
@@ -2,20 +2,21 @@ function getFunc(lenght) {
   const len = lenght;
   let last_pos = 0;
   let total_len = 0;
-  return function(starpos, endpos) {
-    if (starpos<len && endpos < len) {
-      console.log(last_pos, 'last_pos', endpos);
-      total_len = Math.abs(starpos-endpos) +Math.abs(starpos-last_pos);
-      if ( total_len <= 5) {
+  return function (starpos, endpos) {
+    if (starpos < len && endpos < len) {
+      console.log(last_pos, "last_pos", endpos);
+      total_len = Math.abs(starpos - endpos) + Math.abs(starpos - last_pos);
+      if (total_len <= 5) {
         last_pos = endpos;
-        return total_len*100;
+        return total_len * 100;
       }
-      if (total_len > 5 && total_len <=10) {
+      if (total_len > 5 && total_len <= 10) {
         last_pos = endpos;
-        return total_len*150;
-      } if (total_len >10) {
+        return total_len * 150;
+      }
+      if (total_len > 10) {
         last_pos = endpos;
-        return total_len*200;
+        return total_len * 200;
       }
     }
   };
