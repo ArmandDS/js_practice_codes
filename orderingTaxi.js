632bb1ee1ca81781523747511a2e524b5995ab7e
armand <olivares12@hotmail.com>
2021-09-15 07:13:26 -0400

    Ordering Taxi Example

---
+++ orderingTaxi.js
@@ -0,0 +1,33 @@
+function getFunc(lenght) {
+    const len = lenght;
+    let last_pos = 0;
+    let total_len = 0;
+    return function(starpos, endpos) {
+        if(starpos<len && endpos < len){
+            
+            console.log(last_pos, "last_pos", endpos);
+            total_len = Math.abs(starpos-endpos) +Math.abs(starpos-last_pos);
+            if ( total_len <= 5){
+                last_pos = endpos;
+                return total_len*100;
+
+            }
+            if (total_len > 5 && total_len <=10){
+                last_pos = endpos;
+                return total_len*150;
+            }if (total_len >10){
+                last_pos = endpos;
+                return total_len*200
+            }
+            
+        }
+
+    }
+}
+var f = getFunc(20);
+
+console.log(f(0, 3)); 
+
+console.log(f(6, 9));
+
+console.log(f(12, 0));  
